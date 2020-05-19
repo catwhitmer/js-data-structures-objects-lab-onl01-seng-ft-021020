d5d2ac9cc0633748d579d363439218759626367b
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-19 14:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const newdriver = Object.assign({}, driver);
-  
+  delete newdriver[key];
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
